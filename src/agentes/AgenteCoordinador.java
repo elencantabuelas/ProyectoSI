1ac8cb55ede51e30e23a0c72aed0822475f78e89
VicentMorant <vicentmorant03@gmail.com>
2026-05-22 11:32:04 +0200

    add:[Cclase utilidades, fix:paginas amarillas

--- src/agentes/AgenteCoordinador.java
+++ src/agentes/AgenteCoordinador.java
@@ -1,12 +1,19 @@
 package agentes;
 
+import behaviours.ProcesarPlanBehaviour;
 import behaviours.RecibirExamenBehaviour;
 import behaviours.RecibirRespuestasCalculoBehaviour;
 import jade.content.lang.Codec;
 import jade.content.lang.sl.SLCodec;
 import jade.content.onto.Ontology;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import ontologia.OntologiaExamenes;
+
+import static utils.UtilidadesDF.registrarServicio;
 
 public class AgenteCoordinador extends Agent {
 
@@ -20,10 +27,23 @@ public class AgenteCoordinador extends Agent {
 
         System.out.println("AgenteCoordinador iniciado: "+getLocalName());
 
-        //comportamiento de espera de los examenes
+        //llamada al registro
+        registrarServicio(this, "servicio-coordinador", "planificacion-estudios");
+
         addBehaviour(new RecibirExamenBehaviour(this, codec, ontologia));
 
         //comportamiento de espera de los resultados calculados
-        addBehaviour(new RecibirRespuestasCalculoBehaviour(this));
+        //addBehaviour(new RecibirRespuestasCalculoBehaviour(this));
+    }
+
+    @Override
+    protected void takeDown() {
+        //desregistrarse del DF
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        System.out.println("Agente Coordinador (" + getAID().getName() + ") terminando.");
     }
 }
